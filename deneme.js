561ae3eed030c42ee5b2d1c54f4a0e5dbb17d0e4
Furkan Emre YILMAZ <furkan54emre@gmail.com>
2018-01-30 18:49:25 +0300

    Update File

---
+++ deneme.js
@@ -0,0 +1,17 @@
+var runner = "Kendyll";
+var position = 2;
+var medal;
+
+if(position ===1) {
+
+    medal ="gold";
+}
+else if(position === 2) {
+
+    medal = "bronze";
+} else {
+
+    medal = "pat on the back";
+}
+
+console.log(runner + " received + " + medal + " medal");
